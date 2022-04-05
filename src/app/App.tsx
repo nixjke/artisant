dcbc460c5151651fa5f4088a27b36203c052d120
SosiskaPish <aziz.10.03@mail.ru>
2022-04-05 17:00:16 +0300

    add header

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <section className={styles.text}>
+        <h1>Explore</h1>
+        <p>Buy and sell digital fashion NFT art</p>
+      </section>
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
+import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
