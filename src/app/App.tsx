af113d8d20d3afd58198cf45dc92e79cf8cd1097
SosiskaPish <aziz.10.03@mail.ru>
2022-04-07 12:04:46 +0300

    add filtered / available

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,22 +6,32 @@ import Card from '../components/Card/Card'
 
 function App() {
   const dispath = useAppDispatch()
-  const items = useAppSelector(state => state.products)
+  const items = useAppSelector(state => state.products.items)
+  const [checked, setChecked] = React.useState(false)
 
   React.useEffect(() => {
     dispath(fetchProducts())
   }, [])
+
+  const handleChange = () => {
+    setChecked(!checked)
+  }
+
+  const filteredProducts = items.filter(item => item.additional_photos[0]?.compressed?.endsWith('.webp'))
+  const availableProducts = filteredProducts.filter(item => item.quantity_available > 0)
+
 
   return (
     <div className={styles.container}>
       <section className={styles.text}>
         <h1>Explore</h1>
         <p>Buy and sell digital fashion NFT art</p>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
       </section>
       <main className={styles.cards}>
-        {
-          items.items.map(item => (<Card key={item.product_id} items={item} />))
-        }
+        {(checked ? filteredProducts : availableProducts).map(item => (
+          <Card key={item.product_id} item={item} />
+        ))}
       </main>
     </div>
   )
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,19 @@
 import styles from './Card.module.scss'
 
 export default function Card(props: any) {
-  console.log(props.items)
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.img}>
           <img
-            src="https://cdn.artisant.io/api/files/630x819/405_f1ec1f94-8006-452f-99ed-0729f7b65f8d.webp"
+            src={`https://cdn.artisant.io/api/files/630x819/${props.item.additional_photos[0].compressed}`}
             alt="card img"
           />
         </div>
         <div className={styles.content}>
           <div className={styles.author}>
-            <span>{props.items.created_by.display_name}</span>
-            <p>{props.items.name}</p>
+            <span>{props.item.created_by.display_name}</span>
+            <p>{props.item.name}</p>
           </div>
           <div className={styles.name}>Digital Addiction Number Seven</div>
         </div>
@@ -23,12 +22,12 @@ export default function Card(props: any) {
         <div className={styles.available}>
           <p>available</p>
           <span>
-            {props.items.quantity_available} of {props.items.quantity}
+            {props.item.quantity_available} of {props.item.quantity}
           </span>
         </div>
         <div className={styles.price}>
           <p>price</p>
-          <span>{props.items.initial_price}</span>
+          <span>{props.item.initial_price}</span>
         </div>
       </div>
     </div>
